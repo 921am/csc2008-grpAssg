4ade82481328796668052d4db6ec0b7d6e2c3c62
haadee <haadeescott@yahoo.com>
2021-03-16 19:05:58 +0800

    updated 3 main queries
    Run queries in this order
    - create tables
    - create FK
    - seeding data

---
+++ Queries/CreatingTables_Query.sql
@@ -0,0 +1,51 @@
+CREATE TABLE [dbo].[Inmates] (
+    [InmateID]     INT           NOT NULL,
+    [InmateName]   VARCHAR (100) NULL,
+    [Gender]       VARCHAR (1)   NULL,
+    [DateEntered]  DATE          NULL,
+    [DateReleased] DATE          NULL,
+	[DrugOffender] VARCHAR(20) NULL,
+	[InmateProgressID] INT NULL,
+    CONSTRAINT [inmateID] PRIMARY KEY CLUSTERED ([inmateID] ASC)
+
+-- ACADEMIC PROGRAMME
+CREATE TABLE [dbo].[AcadProgramme] (
+    [ProgID]      INT  NOT NULL,
+    [ProgName]    VARCHAR(75)  NULL,
+    [ProgDescrip] VARCHAR(MAX)  NULL,
+    [StartDate]   DATE NULL,
+    [EndDate]     DATE NULL,
+    CONSTRAINT [AcadProgramme_pk] PRIMARY KEY CLUSTERED ([ProgID] ASC)
+);
+
+-- DRUG REHAB PROGRAMME
+CREATE TABLE [dbo].[DrugRehabProgramme] (
+    [ProgID]      INT           NOT NULL,
+    [ProgName]    VARCHAR (75)  NULL,
+    [ProgDescrip] VARCHAR (MAX) NULL,
+    [StartDate]   DATE          NULL,
+    [EndDate]     DATE          NULL,
+    CONSTRAINT [DrugRehabProgramme_pk] PRIMARY KEY CLUSTERED ([ProgID] ASC)
+);
+
+-- VOCATIONAL PROGRAMME
+CREATE TABLE [dbo].[VocationalProgramme] (
+    [ProgID]      INT           NOT NULL,
+    [ProgName]    VARCHAR (75)  NULL,
+    [ProgDescrip] VARCHAR (MAX) NULL,
+    [StartDate]   DATE          NULL,
+    [EndDate]     DATE          NULL,
+    CONSTRAINT [VocationalProgramme_pk] PRIMARY KEY CLUSTERED ([ProgID] ASC)
+);
+
+-- INMATE PROGRESS
+CREATE TABLE [dbo].[InmateProgress] (
+    [InmateProgressID]           INT NOT NULL,
+    [RehabProgID] INT NULL,
+	[RehabProgStatus] VARCHAR (20) NULL,
+	[AcadProgID] INT NULL,
+	[AcadProgStatus] VARCHAR (20) NULL,
+	[VocProgID] INT NULL,
+	[VocProgStatus] VARCHAR (20) NULL,
+    CONSTRAINT [InmateProgress_pk] PRIMARY KEY CLUSTERED ([InmateProgressID] ASC)
+);
