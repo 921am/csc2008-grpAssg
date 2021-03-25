538d0ca1295c00baa361d377b38a90a7b5e4a32c
haadee <haadeescott@yahoo.com>
2021-03-25 22:00:49 +0800

    Updated inmate CRUD

---
+++ DB_WebApp/InmateCRUD.aspx
@@ -0,0 +1,109 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="InmateCRUD.aspx.cs" Inherits="DB_WebApp.InmateCRUD" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:HiddenField ID="hfInmateID" runat="server" />
+            <table>
+                    <tr>
+                    <td>
+                        <asp:Label ID="lblInmateName" runat="server" Text="Name"></asp:Label>
+
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtInmateName" runat="server"></asp:TextBox>
+                    </td>
+                    </tr>
+
+                   <tr>
+                       <td>
+                        <asp:Label ID="lblGender" runat="server" Text="Gender"></asp:Label>
+                        </td>
+                 <%-- change to drop down --%>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtGender" runat="server"></asp:TextBox>
+                    </td>
+                    </tr>
+                <tr>
+                       <td>
+                        <asp:Label ID="lblDateEntered" runat="server" Text="Date Entered"></asp:Label>
+                        </td>
+
+                    <td colspan="2">
+                        <asp:TextBox ID="txtDateEntered" runat="server"></asp:TextBox>
+                    </td>
+                    </tr>
+
+                <tr>
+                       <td>
+                        <asp:Label ID="lblDateReleased" runat="server" Text="Date Released"></asp:Label>
+                        </td>
+
+                    <td colspan="2">
+                        <asp:TextBox ID="txtDateReleased" runat="server" ></asp:TextBox>
+                    </td>
+                    </tr>
+                <%-- change to drop down --%>
+                <tr>
+                       <td>
+                        <asp:Label ID="lblDrugOffender" runat="server" Text="Drug Offender (True/False)"></asp:Label>
+                        </td>
+
+                    <td colspan="2">
+                        <asp:TextBox ID="txtDrugOff" runat="server" ></asp:TextBox>
+                    </td>
+                    </tr>
+                
+                <tr>
+                <td>
+                    
+                </td>
+                <td colspan="2">
+                    <asp:Button ID="btnSaveInmate" runat="server" Text="Save" OnClick="btnSave_Click" />
+                    <asp:Button ID="btnDeleteInmate" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                    <asp:Button ID="btnClearInmate" runat="server" Text="Clear" OnClick="btnClear_Click" />
+                </td>
+            </tr>
+                <tr>
+                <td>
+                    
+                </td>
+                <td colspan="2">
+                    <asp:Label ID="lblSuccessMessage" runat="server" Text="" ForeColor="Green"></asp:Label>
+                </td>
+                <tr>
+                <td>
+                    
+                </td>
+                <td colspan="2">
+                    <asp:Label ID="lblErrorMessage" runat="server" Text="" ForeColor="Red"></asp:Label>
+                </td>
+            </tr>
+            </tr>
+            </table>
+            <br />
+            <asp:GridView ID="gvInmate" runat="server" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField DataField="inmateName" HeaderText="Inmate Name" />
+                <asp:BoundField DataField="gender" HeaderText="Gender" />
+                <asp:BoundField DataField="DateEntered" HeaderText="Entered" />
+                <asp:BoundField DataField="DateReleased" HeaderText="Released" />
+                <asp:BoundField DataField="DrugOffender" HeaderText="Drug Off." />
+
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="lnkViewInmate" runat="server" CommandArgument='<%# Eval("inmateID") %>' OnClick="lnk_OnClick">View</asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DB_WebApp/InmateCRUD.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DB_WebApp
+{
+    public partial class InmateCRUD : System.Web.UI.Page
+    {
+        SqlConnection sqlcon = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CRUD;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                btnDeleteInmate.Enabled = false;
+                FillGridView();
+            }
+        }
+
+        void FillGridView()
+        {
+            if (sqlcon.State == ConnectionState.Closed)
+                sqlcon.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("InmatesViewAll", sqlcon);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dtbl = new DataTable();
+            sqlDa.Fill(dtbl);
+            sqlcon.Close();
+            gvInmate.DataSource = dtbl;
+            gvInmate.DataBind();
+
+        }
+
+        public void Clear()
+        {
+            hfInmateID.Value = "";
+            txtInmateName.Text = "";
+            txtGender.Text = "";
+            txtDateEntered.Text = "";
+            txtDateReleased.Text = "";
+            lblErrorMessage.Text = lblSuccessMessage.Text = "";
+            btnSaveInmate.Text = "Save";
+            btnDeleteInmate.Enabled = false;
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (sqlcon.State == ConnectionState.Closed)
+                sqlcon.Open();
+            SqlCommand sqlCmd = new SqlCommand("InmatesCreateOrUpdate", sqlcon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@inmateID", (hfInmateID.Value == "" ? 0 : Convert.ToInt32(hfInmateID.Value)));
+            sqlCmd.Parameters.AddWithValue("@inmateName", txtInmateName.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@gender", txtGender.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@DateEntered", txtDateEntered.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@DateReleased", txtDateReleased.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@DrugOffender", txtDrugOff.Text.Trim());
+            sqlCmd.ExecuteNonQuery();
+            sqlcon.Close();
+            string UserID = hfInmateID.Value;
+            Clear();
+            if (UserID == "")
+                lblSuccessMessage.Text = "Saved Successfully";
+            else
+                lblSuccessMessage.Text = "Updated Successfully";
+            FillGridView();
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        protected void lnk_OnClick(object sender, EventArgs e)
+        {
+            int inmateID = Convert.ToInt32((sender as LinkButton).CommandArgument);
+            if (sqlcon.State == ConnectionState.Closed)
+                sqlcon.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("InmatesViewByID", sqlcon);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlDa.SelectCommand.Parameters.AddWithValue("@inmateID", inmateID);
+            DataTable dtbl = new DataTable();
+            sqlDa.Fill(dtbl);
+            sqlcon.Close();
+            hfInmateID.Value = inmateID.ToString();
+            txtInmateName.Text = dtbl.Rows[0]["inmateName"].ToString();
+            txtGender.Text = dtbl.Rows[0]["gender"].ToString();
+            txtDateEntered.Text = dtbl.Rows[0]["DateEntered"].ToString();
+            txtDateReleased.Text = dtbl.Rows[0]["DateReleased"].ToString();
+            txtDrugOff.Text = dtbl.Rows[0]["DrugOffender"].ToString();
+            btnSaveInmate.Text = "Update";
+            btnDeleteInmate.Enabled = true;
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (sqlcon.State == ConnectionState.Closed)
+                sqlcon.Open();
+            SqlCommand sqlCmd = new SqlCommand("InmatesDeleteByID", sqlcon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@inmateID", Convert.ToInt32(hfInmateID.Value));
+            sqlCmd.ExecuteNonQuery();
+            sqlcon.Close();
+            Clear();
+            FillGridView();
+            lblSuccessMessage.Text = "Deleted Successfully";
+        }
+    }
+}
