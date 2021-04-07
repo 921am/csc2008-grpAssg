be43dea4f5e859e74fb93f8394cf539c91fa61f2
gohruhui <gohruhui@hotmail.com>
2021-04-07 13:17:14 +0800

    Add Contact Page

--- MongoDB_API/Controllers/HomeController.cs
+++ MongoDB_API/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace WebApplication2.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
